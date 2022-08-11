e040ee22d506cdb0ea02186e75948ee9ec081ac7
Anthony2799 <anthony.johan@hotmail.com>
2022-08-11 20:34:06 -0300

    cambios en todo el front-end

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    letrasCurvas('Desarrollador de apps profecional');
+};
+function letrasCurvas(text){
+    let title = document.getElementById("title");
+    const letras = text.split('')
+  /* for(i=0;i<letras.length;i++){
+        title.innerHTML += `<span style=" transform: rotate(${(i-15)*5}deg);">${letras[i]}</span>`
+    
+    }*/
+}
